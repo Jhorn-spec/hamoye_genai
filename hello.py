888b312beeaa512c3efda904b33cdde1cb904ffa
John <ajayijhorn@yahoo.com>
2024-04-24 14:05:08 +0100

    begin llm

--- hello.py
+++ hello.py
@@ -3,8 +3,9 @@ import openai
 response = openai.ChatCompletion.Create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "user", "content": "Helllo World!"}
+        {"role": "user", 
+        "content": "Helllo World!"}
     ]
 )
 
-print(response['choice'][0]['message']['content'])
+print(response['choice'][0].message.content)
