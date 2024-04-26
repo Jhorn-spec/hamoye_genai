120a034f1de6f64369da68c209002cf4db09d2dc
John <ajayijhorn@yahoo.com>
2024-04-26 12:41:25 +0100

    complet quiz

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
-import openai
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 response = openai.ChatCompletion.Create(
     model="gpt-3.5-turbo",
